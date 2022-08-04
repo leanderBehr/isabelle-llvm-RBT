955fd3672dcf2b65671b83683390a7ed59ba495b
leanderBehr <leandersbehr@gmail.com>
2022-08-04 16:56:41 +0200

    export for benchmark

---
+++ LLVM_DS_RBT/Bench_Export.thy
@@ -0,0 +1,49 @@
+theory Bench_Export
+  imports
+    Free
+    Map_Interface
+    Export_Wrappers
+begin      
+
+
+interpretation bench_rbt: rbt_map
+  ll_icmp_ult       (*less impl*)
+  unat.assn         (*key assn*)
+  "\<lambda>x. Mreturn ()"  (*key delete*)
+  sint.assn         (*value assn*)
+  "\<lambda>x. Mreturn ()"  (*value delete*)
+  "\<lambda>x. Mreturn x"   (*value copy*)
+  "TYPE(nat)"       (*abs key type*)
+  "TYPE(64 word)"   (*key type*)
+  "TYPE(int)"       (*abs value type*)
+  "TYPE(64 word)"   (*value type*)
+  by (standard, vcg)
+
+
+export_llvm
+  bench_rbt.empty_wrap is rbt_empty
+  "M_CONST bench_rbt.free_wrap" is rbt_free
+  "M_CONST bench_rbt.lookup_wrap" is rbt_lookup
+  "M_CONST bench_rbt.insert_wrap" is rbt_insert
+  "M_CONST bench_rbt.delete_wrap" is rbt_delete
+  defines \<open>
+    typedef struct {
+       uint8_t color;
+       rbt_node* lhs;
+       uint64_t key;
+       int64_t value;
+       rbt_node* rhs;
+    } rbt_node;
+    
+    typedef struct {
+      int64_t value;
+      uint8_t flag;
+    } option_i;
+  \<close>
+  rewrites 
+    \<open>(64 word, 64 word) rbt_node\<close> = rbt_node
+    \<open>(64 word) option_i\<close> = option_i
+  file "../exports/rbt.ll"
+
+
+end
